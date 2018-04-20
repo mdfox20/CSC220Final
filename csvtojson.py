594b1de24be97bb978a6bd1037a8114a88175224
Maddy Fox <36060608+mdfox20@users.noreply.github.com>
2018-04-20 12:02:09 -0400

    added python file

---
+++ csvtojson.py
@@ -0,0 +1,45 @@
+import csv
+import json
+
+# Remove unneccesary columns from csv
+with open('paul_ryan.csv','r') as source:
+    reader = csv.reader(source)
+    with open('paul_ryan_fixed.csv','w') as result:
+      writer = csv.writer(result)
+      for r in reader:
+        writer.writerow( (r[3], r[4]) )
+        
+      result.close()
+      source.close()
+
+# Convert CSV to JSON
+csvfile = open('paul_ryan_fixed.csv', 'r')
+
+fieldnames = ("name","amountDonated")
+reader = csv.DictReader(csvfile, fieldnames)
+next(reader);
+
+with open('nodes.json', 'w') as jsonfile:
+  jsonfile.write("[")
+  for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write(', \n')
+  jsonfile.write("]")
+
+  csvfile.close()
+  jsonfile.close()
+
+# Remove trailing comma
+with open('nodes.json', 'r') as fwithcomma:
+  strcomma = fwithcomma.read()
+  i = strcomma.rfind(",")
+  print(i)
+  strnocomma = strcomma[:i] + strcomma[i+1:]
+  print(strnocomma)
+  with open('nodes.json', 'w') as fnocomma:
+    fnocomma.write(strnocomma)
+
+    fwithcomma.close()
+    fnocomma.close()
+    
+  
